fd0356d487c1e41a9a4435e7767cb36e88eaf8ee
saswat <saswatsingh95747@gmail.com>
2024-02-10 23:00:24 +0530

    solved recursion problems on sub sequences

--- striver/recursion/recursion.cpp
+++ striver/recursion/recursion.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
-
+#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 //Reverse an array
@@ -110,11 +111,112 @@ void generateSubset(int index, vector<int>& result, const vector<int>& arr, int 
     generateSubset(index + 1, result, arr, n);
 }
 
-int main() {
-    vector<int> arr{1, 2, 3};
+
+//  Problem statement:
+
+// You are given an array 'nums' of ‘n’ integers.
+
+
+// Return all subset sums of 'nums' in a non-decreasing order.
+void generateSubsetSums(int index, int sum, const vector<int>& nums, vector<int>& subsetSums) {
+    if (index == nums.size()) {
+        subsetSums.push_back(sum);
+        return;
+    }
+
+    // Include the current element and generate subsets
+    generateSubsetSums(index + 1, sum + nums[index], nums, subsetSums);
+
+    // Exclude the current element and generate subsets
+    generateSubsetSums(index + 1, sum, nums, subsetSums);
+}
+
+
+
+vector<int> subsetSums(vector<int>& nums) {
     vector<int> result;
+    generateSubsetSums(0, 0, nums, result);
+    
+    // Sort the subset sums in non-decreasing order
+    sort(result.begin(), result.end());
+    
+    
+    return result;
+}
 
-    generateSubset(0, result, arr, arr.size());
 
-    return 0;
+// Print all Sub sequence which sums to K
+void sum_equal_k(int n,int sum,int s,vector<int> &nums,vector<int> &arr,int i){
+    if(i==n){
+        if(s==sum){
+            for(auto i:nums){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+        }
+            return;
+    }
+
+    nums.push_back(arr[i]);
+    s+=arr[i];
+    sum_equal_k(n,sum,s,nums,arr,i+1);
+
+    s-=arr[i];
+    nums.pop_back();
+    sum_equal_k(n,sum,s,nums,arr,i+1);
+}
+
+
+// Print only one Sub sequence which sums to K
+bool first_sum_equal_K(int n,int sum,int s,vector<int> &result,vector<int> &nums,int i){
+    if(i==n){
+       if(s==sum){
+         for(auto i:result){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        return true;
+       }
+       else return false;
+    }
+
+    result.push_back(nums[i]);
+    s+=nums[i];
+    if(first_sum_equal_K(n,sum,s,result,nums,i+1)==true){
+        return true;
+    };
+    
+    s-=nums[i];
+    result.pop_back();
+    if(first_sum_equal_K(n,sum,s,result,nums,i+1)==true){
+        return true;
+    };
+
+    return false;
+
+}
+
+//print the count of sums of subsequence
+//time complexity is O(n)
+int count_subsequences(int index,int sum,int s,vector<int> nums){
+    if(index==nums.size()){
+        if(s==sum){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    s+= nums[index];
+    int l=count_subsequences(index+1,sum,s,nums);
+
+    s-=nums[index];
+    int r=count_subsequences(index+1,sum,s,nums);
+    return l+r;
+}
+int main() {
+   vector<int> arr{1,2,1};
+   vector<int> results;
+    cout<<count_subsequences(0,2,0,arr);
 }
